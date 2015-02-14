038c2af42f6d9477edad366ac7b4eca39042900d
Michael Jung <mjungg@ucla.edu>
2015-02-14 05:55:35 +0000

    add pseudocode.py

---
+++ pseudocode.py
@@ -0,0 +1,20 @@
+#this script finds all great white shark species in a phylogeny and prunes the tree 
+#then downlaods occurrence data from obis and put onto a map
+
+# greatwhiteshark = species that I want
+# sharktree = shark phylogeny tree in newick format
+
+# def treePrune (greatwhiteshark, sharktree):
+    #return prunedtree = pruned version of shark tree with great white sharks only
+
+#once I get the subtree
+
+# script to feed the occurance data of the GWS from obis
+
+# plot the data points using matplotlib
+#diff color for each species
+
+# calc and plot speciation rates
+
+
+
